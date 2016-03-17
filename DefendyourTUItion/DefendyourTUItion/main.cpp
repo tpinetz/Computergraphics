a5f76ee00eaba5f3834e8c50c3be54364ecd95f4
Thomas Pinetz <Thomas Pinetz>
2016-03-17 20:53:04 +0100

    Created a standard Window Project in OpenGL.

--- DefendyourTUItion/DefendyourTUItion/main.cpp
+++ DefendyourTUItion/DefendyourTUItion/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GL/glfw3.h>
 
 int main() {
-	std::cout << "Hello World!";
+	if (!glfwInit()) {
+		std::cerr << "Test!";
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL 
 	
+	GLFWwindow* window;
+	window = glfwCreateWindow(1024, 768, "Defend your TUItion", NULL, NULL);
+	if (window == NULL) {
+		std::cerr << "Failed to create Window";
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Failed to initialize.";
+		return -1;
+	}
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	do {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
+
+
 	return 0;
 }
